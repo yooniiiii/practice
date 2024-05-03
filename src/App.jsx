43181e1c1000272155b38a3a2b0bce3c4b71df45
yooniiiii <98rla02@naver.com>
2024-05-03 15:00:27 +0900

    움냠냠

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const arr = [
+  {
+    name: "윤희",
+    age: 19,
+    job: "designer",
+  },
+  {
+    name: "연지",
+    age: 25,
+    job: "무직",
+  },
+  {
+    name: "현석",
+    age: 41,
+    job: "잡부",
+  },
+  {
+    name: "장주",
+    age: 50,
+    job: "애아빠",
+  },
+];
+
 function App() {
   const [state, setState] = useState(0);
   const [value, setValue] = useState("");
 
   return (
     <>
-      <section className="border-2 border-red-600 py-2 text-center">
+      <section className="border-2 border-red-800 py-2 text-center">
         {state}
       </section>
       <div className="border-1 border-blue-300 ">
@@ -21,10 +44,26 @@ function App() {
           onChange={(event) => setValue(event.target.value)}
         />
       </div>
-      <button onClick={() => setState(state + 1)}>버튼 클릭</button>
-      <button onClick={() => setState(state - 1)}>버튼 클릭</button>
+      <button className=" text-lg" onClick={() => setState(state + 1)}>
+        버튼 클릭
+      </button>
+      <button className=" text-lg" onClick={() => setState(state - 1)}>
+        버튼 클릭
+      </button>
 
       <span>인풋에 들어가는 값 : {value}</span>
+
+      <ul className="my-10">
+        {arr.map((element, index) => {
+          return (
+            <li className="py-5" key={index}>
+              <p>이름 : {element.name}</p>
+              <span>나이 : {element.age}</span>
+              <p>직업 : {element.job}</p>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
